6223100406e4f539bcecdede59b3a1bf1860e750
paulalfille <paulalfille@penguin>
2024-04-13 09:23:27 -0400

    Genetic algorithm works

--- gen/GA.js
+++ gen/GA.js
@@ -1,82 +1,97 @@
 // Javascript genetic algorithm for solving best volume
 
+// find segment lengths to solve Paul Alfille's HotPaalePi problem
+// Javascript E6+ code
+
+// See https://github.com/alfille/HotApplePi
+
+// Problem constraints:
+// for N+1 segments
+//   all segment lengths: 0 <= u <= Lhat
+//   u[0] = u[N] = 0
+//   | u[i] - u[i+1] | <= 1/N
+
 const Settings = {
-	N:		50, 	// segments
-	Pop:	100, 	// Candidates/generation
-	Lhat:	1.0 	// Relative length
+	N:		100, 	// number of segments
+	Pop:	1000, 	// Candidates/generation
+	Lhat:	1.0 	// Relative length side to (unfolded) width
 } ;
 
-function random_in_range( rng ) {
-	// rng array [lo, hi]
-	return Math.random()*(rng[1]-rng[0]) + rng[0] ;
-}
-
-function penumbra( ua, ub ) {
-	// allowable range between ua, ub
-	const NN = 1/Settings.N ;
-	return [
-		Math.max( 0 , Math.min( ua-NN, ub-NN ) ),
-		Math.min( Settings.Lhat , Math.max( ua+NN, ub+NN ) ),
-		] ;
-}
-
-function close_enough( u, utarget ) {
-	// within proper distance to utarget
-	const NN = 1/Settings.N ;
-	return ( u >= utarget-NN ) && ( u <= utarget+NN ) ;
-}
-
 class Candidate {
+	// Single sequence
 	constructor () {
-		this.value = 0 ;
-		this.u =  Array(Settings.N+1).fill(0.);
+		this.value = 0 ; // calculated volume (Ignores constant multiplier)
+		this.u =  Array(Settings.N+1).fill(0.); // Segment lengths
 		this.mutate_all() ;
 		this.valuate();
 	}
 
 	mutate_all() {
 		for ( let i = 1; i < Settings.N ; ++i ) {
-			this.mutate_i( i ) ;
+			this.mutate_slot( i ) ;
 		}
 	}
 	
-	mutate_i( i ) {
-		this.u[i] = random_in_range( penumbra(this.u[i-1], this.u[i+1]) ) ;
+	mutate_slot( i ) {
+		const NN = 1/Settings.N ;
+		const min = Math.max(             0, this.u[i-1]-NN, this.u[i+1]-NN ) ;
+		const max = Math.min( Settings.Lhat, this.u[i-1]+NN, this.u[i+1]+NN ) ;
+		this.u[i] = Math.random() * ( max-min ) + min ;
 	}
 	
-	copy_from_at_i( C, i ) {
+	copy_at_slot( C, i ) {
 		// copies from another Candidate, but valuation defered
 		for ( let j = i; j<=Settings.N ; ++j ) {
 			this.u[j] = C.u[j] ;
 		}
 	}
 	
-	copy_from( C ) {
-		this.copy_from_at_i( C, 0 ) ;
+	copy( C ) {
+		this.copy_at_slot( C, 0 ) ;
+	}
+	
+	random_slot() {
+		return Math.floor(Math.random() * (Settings.N-1))+1 ;
 	}
 	
 	mutate_from(C) {
-		this.copy_from(C);
-		this.mutate_i(Math.floor(Math.random() * (Settings.N-1))+1) ;
+		this.copy(C);
+		this.mutate_slot(this.random_slot()) ;
 		this.valuate() ;
 	}
 	
+	close_enough( u1, u2 ) {
+		// within proper distance to utarget
+		const NN = 1/Settings.N ;
+		return Math.abs(u1-u2) < NN ;
+	}
+	
 	sex_between( A, B ) {
-		for ( let j = 1 ; j <= Settings.N ; ++j ) {
-			if ( close_enough( A.u[j], B.u[j+1] ) ) {
-				console.log("A->B");
-				this.copy_from( A ) ;
-				this.copy_from_at_i( B, j+1 ) ;
+		// a little complicated:
+		// Copy part of A and rest of B
+		// make sure transition point is random but also legal ("close_enough for a valid derivative).
+		// So start at a random spot and start testing for close_enough
+		// Copy direction depends on which way is close enough.
+		// If no possible transition, just mutate
+		
+		const start = this.random_slot() ;
+		for ( let k = 1 ; k <= Settings.N ; ++k ) {
+			const j = (k+start-1) % Settings.N + 1 ; // need to shift to avoid endpoints
+			if ( this.close_enough( A.u[j], B.u[j+1] ) ) {
+				//console.log("A->B");
+				this.copy( A ) ;
+				this.copy_at_slot( B, j+1 ) ;
 				this.valuate() ;
 				return ;
-			} else if ( close_enough( B.u[j], A.u[j+1] ) ) {
-				console.log("B->A");
-				this.copy_from( B ) ;
-				this.copy_from_at_i( A, j+1 ) ;
+			} else if ( this.close_enough( B.u[j], A.u[j+1] ) ) {
+				//console.log("B->A");
+				this.copy( B ) ;
+				this.copy_at_slot( A, j+1 ) ;
 				this.valuate() ;
 				return ;
 			}
 		}
+		//console.log("Mutate");
 		this.mutate_all() ;
 		this.valuate() ;
 	}
@@ -89,6 +104,7 @@ class Candidate {
 }		 
 
 class Generation {
+	// Holds a list of candidates and manages population selection
 	constructor () {
 		this.population=Array(Settings.Pop).fill(null) ;
 		this.population.forEach( (_,i)=>this.population[i]=new Candidate() ) ;
@@ -97,13 +113,18 @@ class Generation {
 	}
 	
 	resort() {
+		// reorder the list of candidates by fitness
 		this.population.sort((p1,p2)=>p2.value-p1.value);
 		this.best = this.population[0].value ;
 		console.log("Best:",this.best);
 	}
 	
+	volume() {
+		return 4 * this.best ;
+	}
+	
 	mutate() {
-		// Make worst half a mutated copy of best
+		// Make worst half of population a mutated copy of best half
 		for ( let i = this.half; i<this.population.length; ++i ) {
 			this.population[i].mutate_from(this.population[i-this.half]) ;
 		}
@@ -111,6 +132,8 @@ class Generation {
 	}		
 
 	procreate() {
+		// genetically mix with preference for most fit
+		// pairswise best replace lower half, and best children get mixed into procreators
 		for ( let i = 0 ; i < this.half ; ++i ) {
 			this.population[i+this.half].sex_between( this.population[2*i], this.population[2*i+1] ) ;
 		}
