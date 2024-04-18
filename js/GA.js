9d1e33487102e508925699065fba99e45a577098
alfille <paul.alfille@gmail.com>
2024-04-18 15:06:07 -0400

    Update calculation of X folded

--- js/GA.js
+++ js/GA.js
@@ -21,157 +21,6 @@ const Settings = {
 	era:			30,		// number of generations 
 } ;
 
-class Candidate {
-	// Single sequence
-	constructor () {
-		this.value = 0 ; // calculated volume (Ignores constant multiplier)
-		this.u =  Array(Settings.N+1).fill(0.); // Segment lengths
-		this.mutate_all() ;
-		this.valuate();
-	}
-
-	mutate_all() {
-		for ( let i = 1; i < Settings.N ; ++i ) {
-			this.mutate_slot( i ) ;
-		}
-	}
-	
-	mutate_slot( i ) {
-		const NN = 1/Settings.N ;
-		const min = Math.max(             0, this.u[i-1]-NN, this.u[i+1]-NN ) ;
-		const max = Math.min( Settings.Lhat, this.u[i-1]+NN, this.u[i+1]+NN ) ;
-		this.u[i] = Math.random() * ( max-min ) + min ;
-	}
-	
-	copy_at_slot( C, i ) {
-		// copies from another Candidate, but valuation defered
-		for ( let j = i; j<=Settings.N ; ++j ) {
-			this.u[j] = C.u[j] ;
-		}
-	}
-	
-	copy( C ) {
-		this.copy_at_slot( C, 0 ) ;
-	}
-	
-	random_slot() {
-		return Math.floor(Math.random() * (Settings.N-1))+1 ;
-	}
-	
-	mutate_from(C) {
-		this.copy(C);
-		this.mutate_slot(this.random_slot()) ;
-		this.valuate() ;
-	}
-	
-	close_enough( u1, u2 ) {
-		// within proper distance to utarget
-		const NN = 1/Settings.N ;
-		return Math.abs(u1-u2) < NN ;
-	}
-	
-	sex_between( A, B ) {
-		// a little complicated:
-		// Copy part of A and rest of B
-		// make sure transition point is random but also legal ("close_enough for a valid derivative).
-		// So start at a random spot and start testing for close_enough
-		// Copy direction depends on which way is close enough.
-		// If no possible transition, just mutate
-		
-		const start = this.random_slot() ;
-		for ( let k = 1 ; k <= Settings.N ; ++k ) {
-			const j = (k+start-1) % Settings.N + 1 ; // need to shift to avoid endpoints
-			if ( this.close_enough( A.u[j], B.u[j+1] ) ) {
-				//console.log("A->B");
-				this.copy( A ) ;
-				this.copy_at_slot( B, j+1 ) ;
-				this.valuate() ;
-				return ;
-			} else if ( this.close_enough( B.u[j], A.u[j+1] ) ) {
-				//console.log("B->A");
-				this.copy( B ) ;
-				this.copy_at_slot( A, j+1 ) ;
-				this.valuate() ;
-				return ;
-			}
-		}
-		//console.log("Mutate");
-		this.mutate_all() ;
-		this.valuate() ;
-	}
-		
-	valuate() {
-		let val_end = 0 ;
-		let val_L = 0 ;
-		let u0 = u[0] ;
-		const N2 = Settings.N**2 ;
-		u.slice(1).forEach( u1 => {
-			const sq = Math.sqrt(1-N2*(u1-u0)**2);
-			val_L += sq * (u1+u0) ;
-			val_end += u1**2 + u1*u0 + u0**2 ;
-			u0 = u1 ;
-		});
-		this.value = ( 3*Settings.Lhat*val_L - 2*val_end ) / ( 6 * Settings.N) ;
-	}
-}		 
-
-class Generation {
-	// Holds a list of candidates and manages population selection
-	constructor () {
-		this.population=Array(Settings.population).fill(null) ;
-		this.population.forEach( (_,i)=>this.population[i]=new Candidate() ) ;
-		this.half = Math.floor(this.population.length/2) ;
-		this.resort();
-	}
-	
-	resort() {
-		// reorder the list of candidates by fitness
-		this.population.sort((p1,p2)=>p2.value-p1.value);
-		this.best = this.population[0].value ;
-		console.log("Best:",this.best);
-	}
-	
-	volume() {
-		return 4 * this.best ;
-	}
-	
-	mutate() {
-		// Make worst half of population a mutated copy of best half
-		for ( let i = this.half; i<this.population.length; ++i ) {
-			this.population[i].mutate_from(this.population[i-this.half]) ;
-		}
-		this.resort() ;
-	}		
-
-	procreate() {
-		// genetically mix with preference for most fit
-		// pairswise best replace lower half, and best children get mixed into procreators
-		for ( let i = 0 ; i < this.half ; ++i ) {
-			this.population[i+this.half].sex_between( this.population[2*i], this.population[2*i+1] ) ;
-		}
-		this.resort() ;
-	}
-}
-
-class Evolution {
-	constructor () {
-		this.gen = new Generation() ;
-	}
-	
-	run () {
-		for ( let g = 0 ; g<Settings.generations ; ++g ) {
-			this.gen.mutate() ;
-			this.gen.procreate() ;
-		}
-	}
-	
-	all() {
-		for ( let r = 0 ; r<Settings.era ; ++r ) {
-			this.run() ;
-		}
-	}
-}
-
 class Offload {
 	constructor() {
 		this.seq = 0 ; // sequence to keep track of changes
@@ -347,14 +196,15 @@ class CanvasFolded extends CanvasType {
 	}
 	
 	Xs(u) {
-		let temp = 0. ;
-		let X = [0] ;
-		let N1 = 1/(Settings.N*Settings.N) ;
-		for ( let i = 1 ; i <= Settings.N; ++i ) {
-			const dx = Math.sqrt( N1 - (u[i]-u[i-1])**2 ) ;
-			temp += dx ;
-			X.push(temp) ;
-		}
-		return X.map( x => x + (1-temp)/2 ) ; // centering
+		let sum = 0. ;
+		const N1 = 1/Settings.N**2 ;
+		let u0 = u[0] ;
+		const X = u.slice(1).map( u1 => {
+			sum += Math.sqrt(N1-(u1-u0)**2) ;
+			u0=u1;
+			return sum;
+		});
+		X.unshift(0);
+		return X.map( x => x + (1-sum)/2 ) ; // centering
 	}
 }
